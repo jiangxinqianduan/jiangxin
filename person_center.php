86ff3df6912fb88a2ebe92061e8d67d6e5fdf10a
张健宁 <775004030@qq.com>
2016-04-14 22:35:33 +0800

    person_center购物车完成

--- person_center.php
+++ person_center.php
@@ -17,7 +17,9 @@
     
     if(!(isset($_SESSION['username'])&&isset($_SESSION['isLogin'])))
         header("Location:login.php");
-    $user_id=$_SESSION['u_id'];
+    $result=mysqli_query($con,"select u_id,u_money from user where u_name='".$user."'");
+    $row=mysqli_fetch_array($result);
+    $user_id=$row['u_id'];
     ?>
     <div class="container-fluid">
         <div class="person_center_background">
@@ -25,13 +27,13 @@
             <!-- Header End-->
 
             <!-- Content -->
-<?php 
-$type="cart"; 
-if(isset($_GET['type']))
-{
-    $type=$_GET['type'];
-}
-?>
+            <?php 
+            $type="cart"; 
+            if(isset($_GET['type']))
+            {
+                $type=$_GET['type'];
+            }
+            ?>
             <div class="row content-structure text-center">
                 <div class="col-md-2 text-center person-menu">
                     <table class="table table-hover menu-list">
@@ -74,7 +76,6 @@ if(isset($_GET['type']))
                         </tbody>
                     </table>
                 </div>
-                <?php mysqli_query($con,"select u_money from user where u_id=$user_id and u)name=") ?>
                 <div class="col-md-10 person-content">
                     <div class="row login">
                         <div class="col-md-4 text-center">
@@ -82,12 +83,12 @@ if(isset($_GET['type']))
                         </div>
                         
                         <div class="col-md-4 text-center">
-                            <h3>我的余额:--</h3>
+                            <h3>我的余额:￥<?php echo $row['u_money'];?></h3>
                             <h3>我的优惠券:--</h3>
                         </div>
                         <div class="col-md-4 text-center">
                             <h3>我的礼品</h3>
-                            <a href=""><h4>请登录查看</h4></a>
+                            <a href=""><h4></h4></a>
                         </div>
                     </div>
                     <div class="row collect-header text-left">
@@ -95,7 +96,7 @@ if(isset($_GET['type']))
                         <hr align="center" size="3px">
                     </div>
                     <div class="row collect-content text-left">
-                       <ul class="list-inline collect-menu">
+                     <ul class="list-inline collect-menu">
                         <li>分类筛选:</li>
                         <li><a href="">网站导航</a></li>
                         <li><a href="">买家中心</a></li>
@@ -119,205 +120,70 @@ if(isset($_GET['type']))
                         <h3>价格（元）</h3>
                     </div>
                 </div>
-                <div class="row collect-item">
-                    <div class="row collect-item-header">
-                        <div class="col-md-2 text-center">
-                            <input type="checkbox"></input>
-                            <label>取消收藏</label>
+                <?php 
+                $result=mysqli_query($con,"select g_id,g_pic,p_price,g_name,c_time from good join collection using(g_id) where u_name='".$user."';");
+                if($result)
+                while($row=mysqli_fetch_array($result))
+                {
+                    ?>
+                    <div class="row collect-item">
+                        <div class="row collect-item-header">
+                            <div class="col-md-2 text-center">
+                                <input type="checkbox"></input>
+                                <label>取消收藏</label>
+                            </div>
+                            <div class="col-md-6 text-left">
+                            <label>收藏时间：<?php echo $row['c_time']; ?></label>
+                            </div>
                         </div>
-                        <div class="col-md-6 text-left">
-                            <label>收藏时间：2013-01-01</label>
-                        </div>
-                    </div>
-                    <div class="row collect-item-content">
-                        <div class="col-md-2 text-center">
-                            <img src="images/">
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label>太阳花</label>
-                            <label>商品货号：654654</label>
-                        </div>
-                        <div class="col-md-2 text-center">
-                            <p>320</p>
-                        </div>
-                        <div class="col-md-2 text-center">
-                            <button type="button" class="btn btn-primary">加入购物车</button>
-                            <h4>取消收藏</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="row collect-item">
-                    <div class="row collect-item-header">
-                        <div class="col-md-2 text-center">
-                            <input type="checkbox"></input>
-                            <label>取消收藏</label>
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label>收藏时间：2013-01-01</label>
+                        <div class="row collect-item-content">
+                            <div class="col-md-2 text-center">
+                                <img src="<?php echo $row['g_pic'];?>">
+                            </div>
+                            <div class="col-md-6 text-left">
+                                <label><?php $row['g_name'];?></label>
+                                <label>商品货号：<?php $row['g_id'];?></label>
+                            </div>
+                            <div class="col-md-2 text-center">
+                                <p><?php echo $row['g_price'];?></p>
+                            </div>
+                            <div class="col-md-2 text-center">
+                                <button type="button" class="btn btn-primary">加入购物车</button>
+                                <h4>取消收藏</h4>
+                            </div>
                         </div>
                     </div>
-                    <div class="row collect-item-content">
-                        <div class="col-md-2 text-center">
-                            <img src="images/">
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label>太阳花</label>
-                            <label>商品货号：654654</label>
-                        </div>
-                        <div class="col-md-2 text-center">
-                            <p>320</p>
-                            <p>320</p>
-                        </div>
-                        <div class="col-md-2 text-center">
-                            <button type="button" class="btn btn-primary">加入购物车</button>
-                            <h4>取消收藏</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="row collect-item">
-                    <div class="row collect-item-header">
-                        <div class="col-md-2 text-center">
-                            <input type="checkbox"></input>
-                            <label>取消收藏</label>
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label>收藏时间：2013-01-01</label>
-                        </div>
-                    </div>
-                    <div class="row collect-item-content">
-                        <div class="col-md-2 text-center">
-                            <img src="images/">
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label>太阳花</label>
-                            <label>商品货号：654654</label>
-                        </div>
-                        <div class="col-md-2 text-center">
-                            <p>320</p>
-                            <p>320</p>
-                        </div>
-                        <div class="col-md-2 text-center">
-                            <button type="button" class="btn btn-primary">加入购物车</button>
-                            <h4>取消收藏</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="row collect-item">
-                    <div class="row collect-item-header">
-                        <div class="col-md-2 text-center">
-                            <input type="checkbox"></input>
-                            <label>取消收藏</label>
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label>收藏时间：2013-01-01</label>
-                        </div>
-                    </div>
-                    <div class="row collect-item-content">
-                        <div class="col-md-2 text-center">
-                            <img src="images/">
-                        </div>
-                        <div class="col-md-6 text-left">
-                            <label>太阳花</label>
-                            <br><br><br>
-                            <label>商品货号：654654</label>
-                        </div>
-                        <div class="col-md-2 text-center">
-                            <p>320</p>
-                            <p>320</p>
-                        </div>
-                        <div class="col-md-2 text-center">
-                            <button type="button" class="btn btn-primary">加入购物车</button>
-                            <h4>取消收藏</h4>
-                        </div>
-                    </div>                 
-                </div>
-                <div class="row page">
-                    <div class="col-md-2 text-center">
-                        <input type="checkbox"></input>
-                        <label>全选</label>
-                    </div>
-                    <div class="col-md-3 text-left">
-                        <button type="button" class="btn btn-default">取消收藏</button>
-                    </div>
-                    <div class="col-md-4 text-center">
-                        <ul class="page-item list-inline">
-                            <li style="margin-right:4px">
-                                <a href="">上一页</a>
-                            </li>
-                            <li>
-                                <a href="">1</a>
-                            </li>
-                            <li>
-                                <a href="">2</a>
-                            </li>
-                            <li>
-                                <a href="">3</a>
-                            </li>
-                            <li>
-                                <a href="">4</a>
-                            </li>
-                            <li>
-                                <a href="">5</a>
-                            </li>
-                            <li>...</li>
-                            <li style="margin-left:10px">
-                                <a href="">下一页</a>
-                            </li>
-                            <label style="margin-left: 6px">共10页</label>
-                        </ul>
-
-                    </div>
-                    <div class="col-md-3">
-                        <span>到第</span>
-                        <input type="number" value="1" min="1" max="100"></input>
-                        <span>页</span>
-                        <button>确定</button>
-                    </div>
-                </div>
-                <div class="row proposal-header text-left">
-                    <h3>猜你喜欢</h3>
-                </div>
-                <hr>
-                <div class="row proposal-content text-center">
-                 <div class="col-md-3 text-center">
-                    <article class="proposal-main-content">
-                        <img src="images/feature-1_03.png" alt="feature1">
-                        <a href=""><h4 class="proposal-name">uktuktukukukuy</h4></a>    
-                        <br>       
-                        <h4 class="proposal-price">$6.50</h4>
-                        <a href=""><h4 class="proposal-people">(已有56人评价）</h4></a>
-                    </article>
-                </div>  
-                <div class="col-md-3 text-center">
-                    <article class="proposal-main-content">
-                        <img src="images/feature-1_03.png" alt="feature1">
-                        <a href=""><h4 class="proposal-name">uktuktukukukuy</h4></a>    
-                        <br>       
-                        <h4 class="proposal-price">$6.50</h4>
-                        <a href=""><h4 class="proposal-people">(已有56人评价）</h4></a>
-                    </article>
-                </div>  
-                <div class="col-md-3 text-center">
-                    <article class="proposal-main-content">
-                        <img src="images/feature-1_03.png" alt="feature1">
-                        <a href=""><h4 class="proposal-name">uktuktukukukuy</h4></a>    
-                        <br>       
-                        <h4 class="proposal-price">$6.50</h4>
-                        <a href=""><h4 class="proposal-people">(已有56人评价）</h4></a>
-                    </article>
-                </div>  
-                <div class="col-md-3 text-center">
-                    <article class="proposal-main-content">
-                        <img src="images/feature-1_03.png" alt="feature1">
-                        <a href=""><h4 class="proposal-name">uktuktukukukuy</h4></a>    
-                        <br>       
-                        <h4 class="proposal-price">$6.50</h4>
-                        <a href=""><h4 class="proposal-people">(已有56人评价)</h4></a>
-                    </article>
-                </div>  
+                    <?php
+                }
+                ?>
             </div>
         </div>
+            <div class="row proposal-header text-left">
+                <h3>猜你喜欢</h3>
+            </div>
+            <hr>
+
+            <div class="row proposal-content text-center">
+            <?php 
+$result=mysqli_query($con,"select g_pic,g_name,g_price,g_sell from good limit 4");
+while($row=mysqli_fetch_array($result))
+{
+    ?>
+    <div class="col-md-3 text-center">
+                <article class="proposal-main-content">
+                    <img src="<?php echo $row['g_pic'];?>" alt="feature1">
+                    <a href=""><h4 class="proposal-name"><?php echo $row['g_name'];?></h4></a>    
+                    <br>       
+                    <h4 class="proposal-price">￥<?php echo $row['g_price'];?></h4>
+                    <a href=""><h4 class="proposal-people">(已有<?php echo $row['g_sell']; ?>人购买）</h4></a>
+                </article>
+            </div>
+    <?php
+}
+?>  
+        </div>
     </div>
+</div>
 </div>
 
 

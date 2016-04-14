6cd9ae3758c67bb03c3c70187290d89f7940451d
Payon <522983486@qq.com>
2016-04-15 00:08:35 +0800

    首页及商品搜索

--- js/index.js
+++ js/index.js
@@ -1,141 +1,49 @@
 /**
  * Created by zhao.ren on 2016/4/14.
  */
-$("#hot1").mouseover(function(){
-    $("#hot1").animate({
+$(".hot").mouseover(function(){
+    $(this).animate({
         height:'225px',
         width:'329px'
     },"fast");
 });
 
-$("#hot1").mouseout(function(){
-    $("#hot1").animate({
+$(".hot").mouseout(function(){
+    $(this).animate({
         height:'218px',
         width:'313px'
     },"fast");
 });
 
-$("#hot2").mouseover(function(){
-    $("#hot2").animate({
-        height:'225px',
-        width:'329px'
-    },"fast");
-});
-
-$("#hot2").mouseout(function(){
-    $("#hot2").animate({
-        height:'218px',
-        width:'313px'
-    },"fast");
-});
-
-$("#hot3").mouseover(function(){
-    $("#hot3").animate({
-        height:'225px',
-        width:'329px'
-    },"fast");
-});
-
-$("#hot3").mouseout(function(){
-    $("#hot3").animate({
-        height:'218px',
-        width:'313px'
-    },"fast");
-});
-
-$("#feature1").mouseover(function () {
-    $("#feature1").animate({
+$(".feature").mouseover(function () {
+    $(this).animate({
         opacity:0.7
     });
 });
 
-$("#feature1").mouseout(function () {
-    $("#feature1").animate({
+$(".feature").mouseout(function () {
+    $(this).animate({
         opacity:1
     });
 });
 
-$("#feature2").mouseover(function () {
-    $("#feature2").animate({
+$(".latest").mouseover(function () {
+    $(this).animate({
         opacity:0.7
     });
 });
 
-$("#feature2").mouseout(function () {
-    $("#feature2").animate({
+$(".latest").mouseout(function () {
+    $(this).animate({
         opacity:1
     });
 });
 
-$("#feature3").mouseover(function () {
-    $("#feature3").animate({
-        opacity:0.7
-    });
-});
+$('.picture').click(function(){
+       if ($(this).attr("src") == "images/greyheart.PNG"){
+           $(this).attr("src","images/redheart.PNG");
+       } else {
+           $(this).attr("src","images/greyheart.PNG");
+       }
 
-$("#feature3").mouseout(function () {
-    $("#feature3").animate({
-        opacity:1
-    });
-});
-
-$("#feature4").mouseover(function () {
-    $("#feature4").animate({
-        opacity:0.7
-    });
-});
-
-$("#feature4").mouseout(function () {
-    $("#feature4").animate({
-        opacity:1
-    });
-});
-
-
-$("#latest1").mouseover(function () {
-    $("#latest1").animate({
-        opacity:0.7
-    });
-});
-
-$("#latest11").mouseout(function () {
-    $("#latest1").animate({
-        opacity:1
-    });
-});
-
-$("#latest2").mouseover(function () {
-    $("#latest2").animate({
-        opacity:0.7
-    });
-});
-
-$("#latest2").mouseout(function () {
-    $("#latest2").animate({
-        opacity:1
-    });
-});
-
-$("#latest3").mouseover(function () {
-    $("#latest3").animate({
-        opacity:0.7
-    });
-});
-
-$("#latest3").mouseout(function () {
-    $("#latest3").animate({
-        opacity:1
-    });
-});
-
-$("#latest4").mouseover(function () {
-    $("#latest4").animate({
-        opacity:0.7
-    });
-});
-
-$("#latest4").mouseout(function () {
-    $("#latest4").animate({
-        opacity:1
-    });
 });

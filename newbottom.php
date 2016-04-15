064922b8f2f4970ccb3de39f6c7aa0e6e6d0d75b
Pandaice <10142510237@ecnu.cn>
2016-04-15 14:42:54 +0800

    Rename bottom.php to newbottom.php

---
+++ newbottom.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script>
+    <script src="http://apps.bdimg.com/libs/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./css/bottom.css">
+    <title>Title</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="footer-background">
+            <div class="container">
+            <div class="row footer-information">
+                <div class="col-md-4">
+                    <ul class="footer-contact">
+                        <li><img src="images/icon.png"><label class="little-header">联系我们</label></li>
+                        <li><h5>地址：上海市中山北路3663号</h5></li>
+                        <li><h5>联系电话：18221275842</h5></li>
+                        <li><h5>E-mail：sei_xb2014@126.com</h5></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="footer-contact">
+                        <li><h5 class="little-header1">关于我们</h5></li>
+                        <li><h5>产生背景</h5></li>
+                        <li><h5>了解团队</h5></li>
+                        <li><h5>了解公司</h5></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="footer-contact">
+                        <li><h5 class="little-header1">服务</h5></li>
+                        <li><h5>请求帮助</h5></li>
+                        <li><h5>消费反馈</h5></li>
+                        <li><h5>客服咨询</h5></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="footer-contact">
+                        <li><h5 class="little-header1">学习</h5></li>
+                        <li><h5>线上课程</h5></li>
+                        <li><h5>线下活动</h5></li>
+                        <li><h5>联系手艺人</h5></li>
+                    </ul>
+                </div>
+                <div class="col-md-2">
+                    <ul class="footer-contact">
+                        <li><h5 class="little-header1">申请工作</h5></li>
+                        <li><h5>实习</h5></li>
+                        <li><h5>招聘岗位</h5></li>
+                        <li><h5>投递简历</h5></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row other">
+                <div class="col-md-4">
+                    <h5 class="copyright">copyright2012 made by jiangxin</h5>
+                </div>
+                <div class="col-md-4 col-xs-offset-4 text-right">
+                    <h5>Follow us on: &nbsp <img src="images/icon2.jpg"> &nbsp <img src="images/icon3.jpg"> &nbsp <img src="images/icon4.jpg"></h5>
+                    <hr align="center" size="2px">
+                 </div>
+            </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/index.js"></script>
+</body>
+</html>
